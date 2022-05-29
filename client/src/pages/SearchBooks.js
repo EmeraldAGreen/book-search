259a8a1bcef4ce0364b753b08b194ecfc2f48a93
Emerge <egreengirl1@gmail.com>
2022-05-28 21:54:25 -0600

    mutations.js and changes to SearchBooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import { SAVE_BOOK } from '../../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -53,7 +55,8 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
-  const handleSaveBook = async (bookId) => {
+  const handleSaveBook = async ({ bookId }) => {
+    const [bookId, saveBook] = useState('');
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
@@ -64,18 +67,32 @@ const SearchBooks = () => {
       return false;
     }
 
-    try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+    const [saveBook, { error }] = useMutation(SAVE_BOOK);
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const { data } = await saveBook({
+          variables: { bookId, saveBook },
+        });
+  
+        saveBook('');
+      } catch (err) {
+        console.error(err);
       }
+    };
+    // try {
+    //   const response = await saveBook(bookToSave, token);
 
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   // if book successfully saves to user's account, save book id to state
+    //   setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    // } catch (err) {
+    //   console.error(err);
+    // }
   };
 
   return (
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,82 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+# LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+    token
+      user {
+        _id
+        username
+      }
+    }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+# ADD_USER will execute the addUser mutation.
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+# SAVE_BOOK will execute the saveBook mutation.
+    mutation saveBook(
+    $description: String!, 
+    $title: String!, 
+    $bookId: String!, 
+    $image: String!, 
+    $link: String!, 
+    $authors: [String!]) {
+    saveBook( 
+    description: $description
+    title: $title
+    bookId: $bookId
+    image: $image
+    link: $link
+    authors: [$authors]]) {
+      _id
+      description
+      title
+      bookId
+      image
+      link
+      authors
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  # REMOVE_BOOK will execute the removeBook mutation
+  mutation removeBook(
+    $description: String!, 
+    $title: String!, 
+    $bookId: String!, 
+    $image: String!, 
+    $link: String!, 
+    $authors: [String!]) {
+    removeBook( 
+    description: $description
+    title: $title
+    bookId: $bookId
+    image: $image
+    link: $link
+    authors: [$authors]]) {
+      _id
+      description
+      title
+      bookId
+      image
+      link
+      authors
+    }
+  }
+`;
