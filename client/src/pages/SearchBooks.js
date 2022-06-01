20cdbdc6ea8f5aa0e1400b6cb3d5bd389aaf873c
Emerge <egreengirl1@gmail.com>
2022-05-31 21:12:35 -0600

    fix import SAVE_BOOK from mutations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -6,7 +6,7 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
-import { SAVE_BOOK } from '../../utils/mutations';
+import { SAVE_BOOK } from '../utils/mutations';
 import { useMutation } from '@apollo/react-hooks';
 
 const SearchBooks = () => {
