244795507ac6db85a0700ffe885ea5c167c8e245
Emerge <egreengirl1@gmail.com>
2022-06-01 18:21:03 -0600

    remove _id from queries.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,14 +69,15 @@ const SearchBooks = () => {
       try {
         await saveBook({
           variables: { bookData: { ...bookToSave }},
-          update: cache => {
-            const {me} = cache.readQuery({ query: GET_ME });
-            // console.log(me)
-            // console.log(me.savedBooks)
-            cache.writeQuery({ query: GET_ME , data: {me: { ...me, savedBooks: [...me.savedBooks, bookToSave] } } })
-          }
+          // update: cache => {
+          //   const {me} = cache.readQuery({ query: GET_ME });
+          //   // console.log(me)
+          //   // console.log(me.savedBooks)
+          //   cache.writeQuery({ query: GET_ME , data: {me: { ...me, savedBooks: [...me.savedBooks, bookToSave] } } })
+          // }
         });
 
+      console.log(savedBookIds);
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
